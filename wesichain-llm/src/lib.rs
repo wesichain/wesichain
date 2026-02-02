c6b9788d7c87092207ea0ed9e87d7d719a233753
Benediktus Satriya Mukti <bensatriya3@gmail.com>
2026-02-02 10:34:44 +0700

    feat(llm): add Llm trait

--- wesichain-llm/src/lib.rs
+++ wesichain-llm/src/lib.rs
@@ -1,3 +1,9 @@
 mod types;
 
 pub use types::{LlmRequest, LlmResponse, Message, Role, ToolCall, ToolSpec};
+
+use wesichain_core::Runnable;
+
+pub trait Llm: Runnable<LlmRequest, LlmResponse> {}
+
+impl<T> Llm for T where T: Runnable<LlmRequest, LlmResponse> {}
