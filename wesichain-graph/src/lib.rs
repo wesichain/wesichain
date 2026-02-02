193a4ebe5062df0d1da88cfa724d005082247d50
Benediktus Satriya Mukti <bensatriya3@gmail.com>
2026-02-02 23:20:23 +0700

    feat(graph): add execution config defaults

--- wesichain-graph/src/lib.rs
+++ wesichain-graph/src/lib.rs
@@ -1,8 +1,10 @@
+mod config;
 mod checkpoint;
 mod error;
 mod graph;
 mod state;
 
+pub use config::{ExecutionConfig, ExecutionOptions};
 pub use checkpoint::{Checkpoint, Checkpointer, InMemoryCheckpointer};
 pub use error::GraphError;
 pub use graph::{ExecutableGraph, GraphBuilder};
