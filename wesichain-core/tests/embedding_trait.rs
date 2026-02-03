40b5832e1e3e5e70109ea8d2ac468ff79fa794b1
Benediktus Satriya Mukti <bensatriya3@gmail.com>
2026-02-03 17:22:16 +0700

    test(core): cover embedding helpers

--- wesichain-core/tests/embedding_trait.rs
+++ wesichain-core/tests/embedding_trait.rs
@@ -2,7 +2,7 @@ use std::sync::Arc;
 
 use async_trait::async_trait;
 
-use wesichain_core::{Embedding, EmbeddingError};
+use wesichain_core::{embed_batch_ref_dyn, embed_batch_strs_dyn, Embedding, EmbeddingError};
 
 struct TestEmbedding;
 
@@ -13,7 +13,10 @@ impl Embedding for TestEmbedding {
     }
 
     async fn embed_batch(&self, texts: &[String]) -> Result<Vec<Vec<f32>>, EmbeddingError> {
-        Ok(vec![vec![0.0]; texts.len()])
+        Ok(texts
+            .iter()
+            .map(|text| vec![0.0; text.len()])
+            .collect())
     }
 
     fn dimension(&self) -> usize {
@@ -27,4 +30,44 @@ fn assert_object_safe(_embedding: Arc<dyn Embedding>) {}
 fn embedding_trait_is_object_safe() {
     let embedding = Arc::new(TestEmbedding);
     assert_object_safe(embedding);
+}
+
+#[tokio::test]
+async fn embed_batch_strs_helper_works_for_concrete_type() {
+    let embedder = TestEmbedding;
+    let texts = ["hi", "there"];
+    let result = embedder.embed_batch_strs(&texts).await.unwrap();
+    assert_eq!(result, vec![vec![0.0; 2], vec![0.0; 5]]);
+}
+
+#[tokio::test]
+async fn embed_batch_ref_helper_supports_string_inputs() {
+    let embedder = TestEmbedding;
+    let texts = vec!["ab".to_string(), "c".to_string()];
+    let result = embedder.embed_batch_ref(&texts).await.unwrap();
+    assert_eq!(result, vec![vec![0.0; 2], vec![0.0; 1]]);
+}
+
+#[tokio::test]
+async fn embed_batch_ref_helper_supports_str_inputs() {
+    let embedder = TestEmbedding;
+    let texts = vec!["a", "xyz"];
+    let result = embedder.embed_batch_ref(&texts).await.unwrap();
+    assert_eq!(result, vec![vec![0.0; 1], vec![0.0; 3]]);
+}
+
+#[tokio::test]
+async fn embed_batch_helpers_work_for_dyn_embedding() {
+    let embedder: Arc<dyn Embedding> = Arc::new(TestEmbedding);
+    let texts = ["hey", "all"];
+    let result = embed_batch_strs_dyn(embedder.as_ref(), &texts)
+        .await
+        .unwrap();
+    assert_eq!(result, vec![vec![0.0; 3], vec![0.0; 3]]);
+
+    let owned = vec!["one".to_string(), "four".to_string()];
+    let result = embed_batch_ref_dyn(embedder.as_ref(), &owned)
+        .await
+        .unwrap();
+    assert_eq!(result, vec![vec![0.0; 3], vec![0.0; 4]]);
 }
